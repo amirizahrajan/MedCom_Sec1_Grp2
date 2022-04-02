d9466b425401894e071b70aefcc5b3ccc9a0dae8
shireenjan <72268427+shireenjan@users.noreply.github.com>
2022-04-02 13:50:06 -0400

    Pushing Patient and Pharmcy

---
+++ Medcom_Clients/PatientHomePage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medcom_Clients
+{
+    public partial class PatientHomePage : Form
+    {
+        public PatientHomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2CirclePictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PatientHomePage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel6_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Patient_DailyMed p1 = new Patient_DailyMed();
+            p1.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
